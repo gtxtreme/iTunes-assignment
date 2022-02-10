b48d2d1397309df0f430e7aaeaf516717d9fcd59
gtxtreme <gaurav.thakkar@wednesday.is>
2022-02-10 19:14:30 +0530

    Correct syntax

--- app/generate_new_version_code.sh
+++ app/generate_new_version_code.sh
@@ -3,7 +3,7 @@ currentVersionCode=$(awk '/versionCode/ {print $2}' app.gradle)
 
 status=$?
 
-if [ "$status" = 0 ] ; then
+if [ "$status" = 0 ]; then
   echo "Current Version Code : $currentVersionCode"
 else
   echo "Failed to get the current version code. Exiting..."
@@ -15,4 +15,4 @@ echo "Updating CurrentVersionCode by 1"
 let "currentVersionCode=currentVersionCode+1"
 
 echo "New Version Code:$currentVersionCode"
-echo "::setOutput name=NEW_TAG::$currentVersionCode"
+echo "NEW_TAG=$currentVersionCode" >>$GITHUB_ENV
