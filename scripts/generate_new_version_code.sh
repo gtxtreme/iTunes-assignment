5c52df5d2d01e47de3bd43bc08f438de565fc670
gtxtreme <98303832+gtxtreme@users.noreply.github.com>
2022-02-10 18:30:47 +0530

    Add CD (#12)
    * Trigger the CI
    
    * Added new cd.yml
    
    * Added workflow_dispatch to allow manual triggering

---
+++ scripts/generate_new_version_code.sh
@@ -0,0 +1,17 @@
+currentVersionCode=$(awk '/versionCode/ {print $2}' app.gradle)
+
+status=$?
+
+if [ "$status" = 0 ] ; then
+  echo "Current Version Code : $currentVersionCode"
+else
+  echo "Failed to get the current version code. Exiting..."
+  exit 1
+fi
+
+echo "Updating CurrentVersionCode by 1"
+
+let "currentVersionCode=currentVersionCode+1"
+
+echo "New Version Code:$currentVersionCode"
+echo "::setOutput name=NEW_TAG::$currentVersionCode"
