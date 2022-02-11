7cf405d2347672a8fd47af8e6de0806b1a2dc6d1
gtxtreme <gaurav.thakkar@wednesday.is>
2022-02-11 14:06:15 +0530

    Add logic to reflect versionCode changes to files

--- app/generate_new_version_code.sh
+++ app/generate_new_version_code.sh
@@ -1,6 +1,6 @@
 echo "Current working directory $pwd"
-currentVersionCode=$(awk '/versionCode/ {print $2}' app.gradle)
-currentVersionName=$(awk '/versionName/ {print $2}' app.gradle | sed 's/\"//g')
+currentVersionCode=$(awk '/versionCode/ {print $2}' app.gradle) # Regex filter to grab 2 from "versionCode 2"
+currentVersionName=$(awk '/versionName/ {print $2}' app.gradle | sed 's/\"//g') #Regex filter to grab 2.0 from "versionName 2.0"
 status=$?
 
 if [ "$status" = 0 ]; then
@@ -12,10 +12,12 @@ fi
 
 echo "Updating CurrentVersionCode by 1"
 
-let "currentVersionCode=currentVersionCode+1"
+let "currentVersionCode=currentVersionCode+1" # Bumping versionCode By one
 
 echo "New Version Code:$currentVersionCode"
 echo "Version Name: $currentVersionName"
-new_tag="$currentVersionName($currentVersionCode)"
+new_tag="v$currentVersionName($currentVersionCode)" # New tag becomes v1.0(3)
 echo "New Tag: $new_tag"
-echo "NEW_TAG=$new_tag" >>$GITHUB_ENV
+echo "NEW_TAG=$new_tag" >>$GITHUB_ENV # Setting this for use later
+
+sed -i 's/versionCode [0-9a-zA-Z -_]*/versionCode '$currentVersionCode'/' app.gradle # Making changes to files permanent using sed
