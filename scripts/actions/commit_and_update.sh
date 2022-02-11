1868cfc25f988e64b8945699ea290e3953a65e7f
gtxtreme <gaurav.thakkar@wednesday.is>
2022-02-12 01:44:23 +0530

    Add git config stuff

--- scripts/actions/commit_and_update.sh
+++ scripts/actions/commit_and_update.sh
@@ -4,7 +4,6 @@ currentVersionCode=$(awk '/versionCode/ {print $2}' $GITHUB_WORKSPACE/app/app.gr
 let "currentVersionCode=currentVersionCode+1"
 sed -i 's/versionCode [0-9a-zA-Z -_]*/versionCode '$currentVersionCode'/' $GITHUB_WORKSPACE/app/app.gradle
 
-
 # Adding those permanent changes back to version control
 git add $GITHUB_WORKSPACE/app/app.gradle
 git commit -m "Bumped versionCode by 1"
@@ -12,8 +11,10 @@ git commit -m "Bumped versionCode by 1"
 set -e
 
 # Commit and tag this version.
-version=`grep 'version: ' pubspec.yaml | sed 's/version: //'`
+version=$(grep 'version: ' pubspec.yaml | sed 's/version: //')
 
+git config --global user.email "william-the-bot@wednesday.is"
+git config --global user.name "William The Bot"
 git commit -m "Bump version to $version" pubspec.yaml --no-verify
 
 CURRENT_BRANCH=$(git branch --show-current)
